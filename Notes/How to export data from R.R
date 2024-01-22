e4fc5567b7fcf0f6f00e35e9c11e611105de74fb
Enrique Olivares <enrique.sandoval@outlook.de>
2024-01-22 18:10:29 -0500

    He agregado el segundo capítulo del curso

---
+++ Notes/How to export data from R.R
@@ -0,0 +1,9 @@
+#How to export data from R
+#There are several ways to do so, but my preferred one is the following:
+
+write.table(DataToExport, file="File_name.csv", sep=",")
+
+#To a different working directory:
+write.table(DatasetName,
+            file="Users/user/.../DatasetAs.csv",
+            row.names=F, sep=",")
