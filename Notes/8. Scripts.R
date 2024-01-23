5ba1565f73eeb3d0c85a87b8d536be889aa9b2aa
Enrique Olivares <enrique.sandoval@outlook.de>
2024-01-22 22:58:30 -0500

    Added scripts file to the notebook

---
+++ Notes/8. Scripts.R
@@ -0,0 +1,11 @@
+#Scripts are usful as the allow one to easily pick up where they left off on a project and progressively build and refine code and analysis
+#They also allow one to easil reproduce analysis run earlier
+#A script is a set of commands that usually includes commenting on what each piece of code is intended to do
+
+#Attach the data
+attach(online_retail)
+
+#Check the variable names
+names(online_retail)
+
+#Scripts are a way to start and finish your work without you having to write them all daily
