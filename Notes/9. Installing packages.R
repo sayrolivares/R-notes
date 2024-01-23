ea40d35fc96ddf613272c6093725e7ef8972301c
Enrique Olivares <enrique.sandoval@outlook.de>
2024-01-22 23:11:20 -0500

    Added installing packages

---
+++ Notes/9. Installing packages.R
@@ -0,0 +1,18 @@
+#Packages
+#Attach the data
+attach(online_retail)
+
+#Check the variable names
+names(online_retail)
+
+#Install packages
+help(install.packages)
+
+#Install epiR package
+install.packages("epirR")
+
+#Install packages available
+install.packages()
+
+#Load library
+library(epiR)
